8bf45f93abd30b92a8a84f5aad26f97b5a3837e4
muhammadega21 <dermawane988@gmail.com>
2023-10-23 09:25:30 +0700

    tugas 1

---
+++ pertemuan 2/index.js
@@ -0,0 +1,4 @@
+const x = 4;
+const y = 8;
+
+console.log(x * y);
---
+++ tugas 1/script.js
@@ -0,0 +1,9 @@
+const nama = "Muhammad ega dermawan";
+const univ = "Universitas Metamedia";
+const prodi = "Sistem Informasi";
+const nim = 235720111027;
+
+console.log(nama);
+console.log(univ);
+console.log(prodi);
+console.log(nim);
