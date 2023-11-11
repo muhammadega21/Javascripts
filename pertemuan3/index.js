a39ec77a41f4840cc4c53143c7c403a6138467b4
muhammadega21 <dermawane988@gmail.com>
2023-11-11 11:56:03 +0700

    pertemuan 5

---
+++ pertemuan3/index.js
@@ -0,0 +1,15 @@
+mahasiswa = {
+  nama: "ega",
+  kelas: "b",
+  nim: 123,
+};
+
+console.log(mahasiswa.kelas);
+
+function asd(a, b) {
+  var total;
+  total = a * b;
+  return total;
+}
+
+console.log(asd(1, 2));
---
+++ pertemuan5/script.js
@@ -0,0 +1,17 @@
+const a = 8;
+const b = 7;
+
+const penjumlahan = a + b;
+const pengurangan = a - b;
+
+console.log("Penjumlahan : " + a + " + " + b + " = " + penjumlahan);
+console.log("Pengurangan : " + a + " - " + b + " = " + pengurangan);
+
+let penugasan = "Kelas";
+penugasan += "Javascript";
+console.log(penugasan);
+
+const nilai1 = 1;
+const nilai2 = "1";
+console.log(nilai1 == nilai2);
+console.log(nilai1 === nilai2);
