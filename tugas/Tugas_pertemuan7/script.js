a68755415a0ead22e852733e8e2961f666fa62e1
muhammadega21 <dermawane988@gmail.com>
2023-11-25 11:31:33 +0700

    Tugas Pertemuan 7

---
+++ tugas/Tugas_pertemuan7/script.js
@@ -0,0 +1,9 @@
+// Soal 1
+for (let i = 1; i <= 20; i++) {
+  console.log(i + ". Saya berada di kelas kafekoding hari ini");
+}
+
+// Soal 2
+for (let i = 0; i <= 15; i++) {
+  console.log("Saya belajar ke " + i + " kali");
+}
