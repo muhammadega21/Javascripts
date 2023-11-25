4c571755677cdb73dd41bac42074935dd3e42e68
muhammadega21 <dermawane988@gmail.com>
2023-11-25 10:35:29 +0700

    log

--- tugas/Tugas_Pertemuan6/script.js
+++ tugas/Tugas_Pertemuan6/script.js
@@ -1,17 +1,17 @@
-let nama = prompt("Masukkan Nama Anda");
+let nama = "salma";
 
 if (nama == "salma") {
-  alert(
+  console.log(
     "Halo " +
       nama +
       " , anda mengambil kelas Javascript. Kelas anda berada di labor 103, kelas dimulai jam 10.00"
   );
 } else if (nama == "andre") {
-  alert(
+  console.log(
     "Halo " +
       nama +
       ", anda mengambil kelas Design Grafis. Kelas anda berada di labor 101, kelas dimulai jam 13.00"
   );
 } else {
-  alert("Maaf " + nama + " , anda belum mengambil kelas di Kafekoding");
+  console.log("Maaf " + nama + " , anda belum mengambil kelas di Kafekoding");
 }
