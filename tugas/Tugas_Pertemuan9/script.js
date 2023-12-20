3d12e3729b39fb826bb61b30b910965a7fa0a5b4
muhammadega21 <dermawane988@gmail.com>
2023-12-20 13:25:27 +0700

    Tugas Pertemuan 9

---
+++ tugas/Tugas_Pertemuan9/script.js
@@ -0,0 +1,68 @@
+const perusahaan = {
+  struktur: {
+    direksi: {
+      Rosid: {
+        nip: "12345678",
+        noHp: "085763000486",
+        alamat: {
+          jalan: "Jl. M. Syafei",
+          kota: "Payakumbuh",
+          provinsi: "Sumatera Barat",
+        },
+      },
+    },
+    manager: {
+      Surya: {
+        nip: "87654321",
+        noHp: "082172391038",
+        alamat: {
+          jalan: "Jl. Raflesia",
+          kota: "Payakumbuh",
+          provinsi: "Sumatera Barat",
+        },
+      },
+    },
+    divisi: {
+      divisi_pelayanan: {
+        Andre: {
+          nip: "02849271",
+          noHp: "082839103728",
+          alamat: {
+            jalan: "Jl. Khatib Sulaiman",
+            kota: "Padang",
+            provinsi: "Sumatera Barat",
+          },
+        },
+        Akbar: {
+          nip: "47392017",
+          noHp: "084820174629",
+          alamat: {
+            jalan: "Jl. Jaksa Agung R.Soeprato",
+            kota: "Padang",
+            provinsi: "Sumatera Barat",
+          },
+        },
+      },
+      divisi_pemasaran: {
+        Abdul: {
+          nip: "38104750",
+          noHp: "084738291047",
+          alamat: {
+            jalan: "Jl. Teratai",
+            kota: "Padang",
+            provinsi: "Sumatera Barat",
+          },
+        },
+        Rosa: {
+          nip: "38294068",
+          noHp: "082910486710",
+          alamat: {
+            jalan: "Jl. Tol JKT - Tanggerang",
+            kota: "Jakarta",
+            provinsi: "Jawa Barat",
+          },
+        },
+      },
+    },
+  },
+};
