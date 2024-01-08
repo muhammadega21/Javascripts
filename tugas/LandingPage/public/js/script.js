d5ceaa67b58ecffecee91a38f9d55da6f9285034
muhammadega21 <dermawane988@gmail.com>
2024-01-08 20:08:28 +0700

    animasi scroll

--- tugas/LandingPage/public/js/script.js
+++ tugas/LandingPage/public/js/script.js
@@ -1,2 +1,34 @@
-const map = document.querySelector("#map");
-map.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.301120245057!2d100.35712407407851!3d-0.9225609353349017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd4b8b50f84e2df%3A0x6b663fb549733102!2sUniversitas%20Metamedia!5e0!3m2!1sid!2sde!4v1704634063061!5m2!1sid!2sde" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+// map
+const map = document.getElementById("map");
+map.innerHTML =
+  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.3011142301625!2d100.35969899999999!3d-0.9225663000000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd4b8b50f84e2df%3A0x6b663fb549733102!2sUniversitas%20Metamedia!5e0!3m2!1sid!2sde!4v1704718476932!5m2!1sid!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+// navbar
+let section = document.querySelectorAll("section");
+let navlink = document.querySelectorAll("nav .nav-link ul a");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlink.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("nav .nav-link ul a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+// card fade up
+const cards = document.querySelectorAll(".card");
+cards.forEach((card, i) => {
+  card.setAttribute("data-aos", "fade-up");
+  card.setAttribute("data-aos-duration", 1000);
+  card.setAttribute("data-aos-offest", 0);
+});
